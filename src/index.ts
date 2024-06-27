efce402f7a9c0dff7978cc41ef6b5ac64c202bb5
Jonas <jonas.mittemayr@htl-wels.at>
2024-06-27 12:05:04 +0200

    feat: add router config with example user

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import express from 'express';
 import dotenv from 'dotenv';
 import envSchema from './util/env.util';
 
 import envUtil from './util/env.util';
+import router from "./routes";
+
+
+const app = express ();
+
+app.use(express.json());
+
+app.use("/", router);
+
+
+app.listen(envUtil.PORT, () => { console.log("Server started"); })
 
 
 
